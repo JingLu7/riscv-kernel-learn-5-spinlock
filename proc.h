de6328ac0e0e27fef45d9c43aa5707e88cf7e811
jinlu <2323084460@qq.com>
2026-06-05 13:40:32 +0800

    update

--- proc.h
+++ proc.h
@@ -1,5 +1,3 @@
-#ifndef PROC_H
-#define PROC_H
 #include "types.h"
 #include "spinlock.h"
 
@@ -27,7 +25,9 @@ struct proc {
     enum procstate state;
     int pid;
     uint64 kstack;
+    pagetable_t pagetable;
     struct context context;
+    struct trapframe *trapframe;
 };
 
 #define NPROC 4
@@ -43,4 +43,42 @@ struct cpu {
 #define NCPU 1
 extern struct cpu cpus[NCPU];
 
-#endif
+
+struct trapframe {
+    uint64 kernel_stap; // 内核页表
+    uint64 kernel_sp;   // 栈
+    uint64 kernel_trap; // usertrap
+    uint64 epc; // 用户态程序返回
+    uint64 kernel_hartid;
+    uint64 ra; /* 40*/
+    uint64 sp; // 用户栈
+    uint64 gp;
+    uint64 tp;
+    uint64 t0;
+    uint64 t1;
+    uint64 t2;
+    uint64 s0;
+    uint64 s1;
+    uint64 a0;
+    uint64 a1;
+    uint64 a2;
+    uint64 a3;
+    uint64 a4;
+    uint64 a5;
+    uint64 a6;
+    uint64 a7;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
+    uint64 t3;
+    uint64 t4;
+    uint64 t5;
+    uint64 t6;
+};
